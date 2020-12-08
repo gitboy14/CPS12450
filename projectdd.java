b599856586a11bea060d871b6bfb72fdbec39939
gitboy14 <72329538+gitboy14@users.noreply.github.com>
2020-12-08 15:02:34 +0800

    Final

--- projectdd.java
+++ projectdd.java
@@ -1,4 +1,4 @@
-package tool;
+package Projectdd;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -6,30 +6,27 @@ import java.util.regex.Pattern;
 
 //Input the copy of course information from keanwise, and get a neat course data :)
 
-public class Tool {
+public class Projectdd {
 	
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		
 		System.out.println("Please input the courses: ");
 		String text = input.nextLine();
 		
-		for(String[] a: format(text)) {
-			for(String s:a)
-				System.out.println(s);
-		}
-		//Traverse and print
+		String[][] matrix = getMatrix(text);
+		print(matrix);
 	}
 
-	public static String[][] format(String t) {
+
+	public static String[][] getMatrix(String t) {
 		
-		String[][] courses = new String[10][2];
+		String[][] m = new String[10][2];
 		//HERE IS THE DOUBLE-DIMENSIONAL ARRAY !!!
 		
 		Pattern name = Pattern.compile(".{8}\\*W..");
-		Pattern period = Pattern.compile("Lecture[\\s\\S]+?M\\,");
 		//"CPS*2231*W04"
+		Pattern period = Pattern.compile("Lecture[\\s\\S]+?M\\,");
 		//"Lecture Monday, Thursday 04:00PM - 06:45PM"
 		
 		Matcher mName = name.matcher(t);
@@ -37,16 +34,25 @@ public class Tool {
 		
 		int i = 0;		
 		while (mName.find()) {						
-			courses[i][0] = mName.group();
+			m[i][0] = mName.group();
 			i++;
 		}
 		
 		int j = 0;
 		while (mPeriod.find()) {						
-			courses[j][1] = mPeriod.group();
+			m[j][1] = mPeriod.group();
 			j++;
 		}
 		
-		return courses;
+		return m;
 	}
+	
+	public static void print(String[][] m) {
+		
+		for(int i = 0; m[i][0] != null; i++) {
+			System.out.println(m[i][0]);
+			System.out.println(m[i][1]);
+		}
+	}
+		
 }
